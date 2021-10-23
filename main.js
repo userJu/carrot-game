e1a92eabc5171f48f0ecfa5e2aa71bfa6f3e4e1e
JuhyunLim <87933367+userJu@users.noreply.github.com>
2021-10-23 17:13:52 +0900

    JS: 내가 기본적으로 만든 부분
    버튼 누르면 모양 바뀌고 시간 흐르기.
    +
    랜덤 위치에 당근과 벌레 생성

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const startBtn = document.querySelector('.game__startBtn');
 const play = `<i class="fas fa-play"></i>`
 const stop = `<i class="fas fa-stop"></i>`
 const gameTime = document.querySelector('.game__time')
-
+const gameScore = document.querySelector('.game__numOfCarrots')
+const ground = document.querySelector('.ground');
+const groundRect = ground.getBoundingClientRect();
 
 // 1. 시작 버튼
 startBtn.addEventListener('click', ()=>{
@@ -12,11 +14,11 @@ startBtn.addEventListener('click', ()=>{
 let timeLeft = 10
 
 
-
 // - 버튼을 누르면 버튼의 모양이 바뀐다 < toggle 이용하기 
 function changeShape(){
     if(startBtn.innerHTML === play){
         startBtn.innerHTML = `${stop}`
+        randomItem()
     }else if (startBtn.innerHTML === stop){
         startBtn.innerHTML = `${play}`
     }
@@ -28,25 +30,21 @@ function setGameTime(){
     timeLeft--
     if(timeLeft === -1 ){
         console.log('게임 끝')
-        clearInterval(intervalTime)
     }
 }
-const intervalTime = setInterval(setGameTime, 1000)
-
+function intervalFx (){
+        const interval = setInterval(setGameTime, 1000)
+        interval
+}
+console.log(setGameTime)
 
 
 // - 버튼을 누르면 벌레와 당근이 무작위로 ground에 생성된다
-const ground = document.querySelector('.ground');
-const groundRect = ground.getBoundingClientRect();
-function randomItem(){
-    console.log(groundRect);
 
+function randomItem(){
     addItem('carrot', 5, 'img/carrot.png', 80)
     addItem('bug', 5, 'img/bug.png', 50)
-
-
 }
-randomItem()
 
 function addItem (className, count, imgPath, size){
     const width = groundRect.width
@@ -56,26 +54,20 @@ function addItem (className, count, imgPath, size){
         item.src=imgPath
         item.setAttribute('class', className)
         ground.appendChild(item)
-        const x = Math.floor(Math.random()*width)
-        const y = Math.floor(Math.random()*height)
-        console.dir(item)
+        const x = Math.floor(Math.random()*(width-size))
+        const y = Math.floor(Math.random()*(height-size))
         item.style.left = `${x}px`
         item.style.top = `${y}px`
-
-        console.log(x, y)
-
     }
-    console.log(ground)
 }
 
 
 
 // 2. 벌레와 당근  
-// - 벌레를 누르면 실패했다는 알람이 뜬다
 // - 당근을 누르면 당근의 개수가 줄어드는 것이 표시된다
 // - 제한 시간 내에 당근을 다 누르면 성공했다는 알람이 뜬다
 
 
 
 // 3. 제한시간
-// - 제한시간이 지나면 실패했다는 알람이 뜬다
+// - 제한시간이 지나면 알람이 뜬다
